9a02a83e180c31679b891a245ef601bf1865200e
Luca Mazzanti <lucamazzanti83@gmail.com>
2021-01-31 20:19:35 +0100

    Managed client errors to retrow a better status code

--- ShakespearePokemon.API/Controllers/ErrorController.cs
+++ ShakespearePokemon.API/Controllers/ErrorController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ShakespearePokemon.API.Controllers
 {
@@ -7,6 +10,22 @@ namespace ShakespearePokemon.API.Controllers
     public class ErrorController : ControllerBase
     {
         [Route("/error")]
-        public IActionResult Error() => Problem();
+        public IActionResult Error()
+        {
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            if (context.Error is HttpRequestException exception)
+            {
+                HttpRequestException httpRequestException = exception;
+
+                // external consumed API server errrors will be converted in ServiceUnavailable or maybe in future in BadGateway
+                if (httpRequestException.StatusCode != null && (int) httpRequestException.StatusCode >= 500)
+                {
+                    return Problem(statusCode: StatusCodes.Status503ServiceUnavailable);
+                }
+            }
+
+            return Problem();
+        }
     }
 }
